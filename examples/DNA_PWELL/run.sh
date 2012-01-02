90e3f013fe2bf5c0dc9f5d94994040dafb87eddb
Litvinov Sergey <slitvinov@gmail.com>
2012-01-02 02:10:18 +0100

    Add run.sh

---
+++ examples/DNA_PWELL/run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+N=60
+pwell_A=1e-5
+awk -v N=${N} -f scripts/polygen.awk > examples/DNA_PWELL/dna_pwell.str
+../../src/bd_box  dna_pwell.prm \
+--xyz_filename=dna_pwell.xyz \
+--dcd_filename=dna_pwell.dcd \
+--enr_filename=dna_pwell.enr \
+--str_filename=dna_pwell.str \
+--out_filename=dna_pwell.out \
+--pqr_filename=dna_pwell.pqr \
+--pwell_A=0.0
