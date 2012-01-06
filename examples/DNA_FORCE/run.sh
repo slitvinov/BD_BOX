48993e8812355658c068ebf148382fcc72b93f7a
Litvinov Sergey <slitvinov@gmail.com>
2012-01-06 16:22:51 +0100

    Run with well

--- examples/DNA_FORCE/run.sh
+++ examples/DNA_FORCE/run.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-np=1
+np=8
 vel_grad=1.16e-5
 
-seq 1 5 | ${HOME}/bin/parallel -j ${np} --verbose ./runone.sh {} ${vel_grad} 1e-3
+seq 1 32 | ${HOME}/bin/parallel -j ${np} --verbose ./runone.sh {} ${vel_grad} 1e-3
 
 
