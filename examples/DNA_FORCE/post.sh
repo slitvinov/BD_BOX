adf69e438b11a8686cddc0ac9159c010eb0945a2
Litvinov Sergey <slitvinov@gmail.com>
2012-01-06 16:40:13 +0100

    Fix bug in post.sh

--- examples/DNA_FORCE/post.sh
+++ examples/DNA_FORCE/post.sh
@@ -6,17 +6,17 @@ source vars.sh
 
 function getparlist() {
     local pat=$1
-    ls *.xyz -1 | xargs -I{} -n1 basename {} .xyz  | awk -v RS="^" '/${pat}/' | sed 's/${pat}//g' | sort | uniq
+    ls *.xyz -1 | xargs -I{} -n1 basename {} .xyz  | awk -v RS="^" 1 | awk "/${pat}/" | sed "s/${pat}//"
 }
 
+pat=pwell_A
 ls *.xyz | parallel  --verbose ./postonefile.sh {}
 
-pat=pwell_A
 for val in $(getparlist ${pat}); do
-    awk -f ${bddir}/scripts/av.awk dna_pwell.proc*${pat}${val}*.fr-corr-X  > dna_pwell.${pat}${val}fr-corr-X
-    awk -f ${bddir}/scripts/av.awk dna_pwell.proc*${pat}${val}*.fr-corr-Y  > dna_pwell.${pat}${val}fr-corr-Y
-    awk -f ${bddir}/scripts/av.awk dna_pwell.proc*${pat}${val}*.msd  > dna_pwell.${pat}${val}msd
-    awk -f ${bddir}/scripts/av.awk dna_pwell.proc*${pat}${val}*.msdcm  > dna_pwell.${pat}${pat}msdcm
+    awk -f ${bddir}/scripts/av.awk dna_pwell^proc*${pat}${val}*.fr-corr-X  > dna_pwell.${pat}${val}fr-corr-X
+    awk -f ${bddir}/scripts/av.awk dna_pwell^proc*${pat}${val}*.fr-corr-Y  > dna_pwell.${pat}${val}fr-corr-Y
+    awk -f ${bddir}/scripts/av.awk dna_pwell^proc*${pat}${val}*.msd  > dna_pwell.${pat}${val}msd
+    awk -f ${bddir}/scripts/av.awk dna_pwell^proc*${pat}${val}*.msdcm  > dna_pwell.${pat}${pat}msdcm
     scp dna_pwell.${pat}${val}fr-corr-[XY] litvinov@kana:~/Dropbox/
     scp dna_pwell.${pat}${val}msd litvinov@kana:~/Dropbox/
 done
