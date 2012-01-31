66dc4e6488947fdefbffb0ad0b1dee7ef4e1078f
Litvinov Sergey <slitvinov@gmail.com>
2012-01-31 10:45:53 +0100

    Add DNA_DUMBBELL

---
+++ examples/DNA_DUMBBELL/runone.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -e
+set -u
+set -x
+
+source vars.sh
+
+iproc=$1
+N=$2
+
+id=dna_free~proc.${iproc}
+seed=$RANDOM
+printf "seed: %s\n" ${seed} > "/dev/stderr"
+printf "id: %s\n" ${id} > "/dev/stderr"
+awk -v N=${N} -f ${bddir}/scripts/polygen.awk > ${id}.str
+
+time ${bddir}/src/bd_box dna_free.prm \
+    --xyz_filename=${id}.xyz \
+    --str_filename=${id}.str \
+    --out_filename=${id}.out \
+    --dcd_filename=${id}.dcd \
+    --rand_seed=${seed} \
+
+printf "stop: %s\n" ${id} > "/dev/stderr"
