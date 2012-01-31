c4f732c6aca9314037947a72dea5f270a9446cb0
Litvinov Sergey <slitvinov@gmail.com>
2012-01-31 11:11:33 +0100

    Remove xyz output

--- examples/DNA_FREE/run.sh
+++ examples/DNA_FREE/run.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 
 np=1
-N=76
+N=60
 seq 1 1 | ${HOME}/bin/parallel -j ${np} --verbose ./runone.sh {}  ${N}
 
 
--- examples/DNA_FREE/runone.sh
+++ examples/DNA_FREE/runone.sh
@@ -11,10 +11,10 @@ N=$2
 id=dna_free~proc.${iproc}
 seed=$RANDOM
 printf "seed: %s\n" ${seed} > "/dev/stderr"
-echo -n "${id}" > "/dev/stderr"
+printf "id: %s\n" ${id} > "/dev/stderr"
 awk -v N=${N} -f ${bddir}/scripts/polygen.awk > ${id}.str
+
 ${bddir}/src/bd_box dna_free.prm \
-    --xyz_filename=${id}.xyz \
     --str_filename=${id}.str \
     --out_filename=${id}.out \
     --dcd_filename=${id}.dcd \
