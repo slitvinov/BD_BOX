f8046b7e0d9e5faeb64a8bb67b3c6c7747bc5c9b
Litvinov Sergey <slitvinov@gmail.com>
2012-02-03 16:16:21 +0100

    Add a setupt for free polymer simulations

---
+++ examples/DNA_SIM_FREE/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+np=8
+#seq 1 8 | parallel -j ${np} ./runone.sh {} 20
+#seq 1 8 | parallel -j ${np} ./runone.sh {} 40
+#seq 1 8 | parallel -j ${np} ./runone.sh {} 80
+seq 1 8 | parallel -j ${np} ./runone.sh {} 160
+seq 1 8 | parallel -j ${np} ./runone.sh {} 320
+
