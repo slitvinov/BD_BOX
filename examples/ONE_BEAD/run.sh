35067061a28a11119a13589c947e00758b80db7a
Litvinov Sergey <slitvinov@gmail.com>
2012-02-08 16:08:59 +0100

    Update of one bead simulation

--- examples/ONE_BEAD/run.sh
+++ examples/ONE_BEAD/run.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
 np=8
-#seq 1 8 | parallel -j ${np} ./runone.sh {} 20
-#seq 1 8 | parallel -j ${np} ./runone.sh {} 40
-#seq 1 8 | parallel -j ${np} ./runone.sh {} 80
-#seq 1 8 | parallel -j ${np} ./runone.sh {} 160
-seq 1 8 | parallel -j ${np} ./runone.sh {} 60
+seq 1 1 | parallel --verbose -j ${np} ./runone.sh {} 1
 
--- examples/ONE_BEAD/runone.sh
+++ examples/ONE_BEAD/runone.sh
@@ -8,16 +8,16 @@ source vars.sh
 iproc=$1
 N=$2
 
-id=dna_free~proc${iproc}~nb${N}
+id=dna_nb${N}
 seed=$RANDOM
 printf "seed: %s\n" ${seed} > "/dev/stderr"
 printf "id: %s\n" ${id} > "/dev/stderr"
-awk -v N=${N} -f ${bddir}/scripts/polygen.awk > ${id}.str
+#awk -v N=${N} -f ${bddir}/scripts/polygen.awk > ${id}.str
 
 ${bddir}/src/bd_box dna_free.prm \
     --str_filename=${id}.str \
     --out_filename=${id}.out \
-    --dcd_filename=${id}.dcd \
+    --xyz_filename=${id}.xyz \
     --rand_seed=${seed} \
 
 printf "stop: %s\n" ${id} > "/dev/stderr"
