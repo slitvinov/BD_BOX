650f9999aeffcfd14000cb93f01164bae190756e
Litvinov Sergey <slitvinov@gmail.com>
2012-02-08 13:50:27 +0100

    Set up a template for one bead simulation

---
+++ examples/ONE_BEAD/runone.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+set -e
+set -u
+set -x
+
+source vars.sh
+
+iproc=$1
+N=$2
+
+id=dna_free~proc${iproc}~nb${N}
+seed=$RANDOM
+printf "seed: %s\n" ${seed} > "/dev/stderr"
+printf "id: %s\n" ${id} > "/dev/stderr"
+awk -v N=${N} -f ${bddir}/scripts/polygen.awk > ${id}.str
+
+${bddir}/src/bd_box dna_free.prm \
+    --str_filename=${id}.str \
+    --out_filename=${id}.out \
+    --dcd_filename=${id}.dcd \
+    --rand_seed=${seed} \
+
+printf "stop: %s\n" ${id} > "/dev/stderr"
